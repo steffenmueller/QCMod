4244dd88e427a63f4feecd363c8be8e491667689
Steffen Müller <jansteffenmueller@gmail.com>
2021-12-28 15:46:44 +0100

    Added precision tracking

--- frobenius.m
+++ frobenius.m
@@ -3,24 +3,27 @@
 // Functions for computing Frobenius structures //
 //////////////////////////////////////////////////
 
+// July 21: JSM/JB added precision estimates
 // 17/04/20: tried to extend this to allow for slightly worse singularities (in Tuitman's notation e.g. the denominators of the entries of W0 not being divisible by r). Not sure that it's worked.
 
 // 16/04/20: To get this to work when the model is not smooth, we have to change how we compute basisR, phiomega, phiomegaZf and g0omega (which were originally written assuming that b^0 = [1,y,...,y^(d-1)] or equivalently that W0 is the identity. This just involves selectively multiplying by W0 or its inverse. This also means we have to do some additional reductions (i.e. quotient out by z=r/LeadingCoefficient(r). This is done using radix_reduce.
 
-frob_struc:=function(data,Z,eta,bpt)
+frob_struc:=function(data,Z,eta,bpt : N:=0)
 
   // Compute the matrix G of the (inverse) Frobenius structure on A_Z.
 
-  Q:=data`Q; p:=data`p; N:=data`N; g:=data`g; W0:=data`W0; Winf:=data`Winf; f0list:=data`f0list; finflist:=data`finflist; fendlist:=data`fendlist; 
+  Q:=data`Q; p:=data`p; g:=data`g; W0:=data`W0; Winf:=data`Winf; f0list:=data`f0list; finflist:=data`finflist; fendlist:=data`fendlist; 
   FH1U:=data`F; Nmax:=data`Nmax; basis:=data`basis; G0:=data`G0; Ginf:=data`Ginf; red_list_fin:=data`red_list_fin; red_list_inf:=data`red_list_inf; 
-  basis:=data`basis; integrals:=data`integrals; quo_map:=data`quo_map; r:=data`r; Delta:=data`Delta; s:=data`s;
+  basis:=data`basis; integrals:=data`integrals; quo_map:=data`quo_map; r:=data`r; Delta:=data`Delta; s:=data`s; frobmatb0r := data`frobmatb0r; 
+  if IsZero(N) then N:=data`N; end if;
 
   d:=Degree(Q); lc:=LeadingCoefficient(Delta);
 
-  O,Ox,S,R:=getrings(p,Nmax); // O = IntegerRing(p^Nmax), Ox = O[x], S = Ox[z,1/z], R = S[y]
+  O,Ox,S,R:=getrings(p,Nmax); 
+  // O = IntegerRing(p^Nmax), Ox = O[x], S = Ox[z,1/z], R = S[y]
 
-  //  ChangeRing(W0,S);
-  W0inv:=W0^(-1);
+  W0inv:=W0^(-1); // By assumption, W0 and W0inv are p-integral; same for LC(r)
+  W:=Winf*W0^(-1);
   W0S:=ZeroMatrix(S,d,d);
   W0invS:=ZeroMatrix(S,d,d);
   for i in [1..d] do
@@ -47,8 +50,8 @@ frob_struc:=function(data,Z,eta,bpt)
           end if;
       end for;
   end for;
-      // Coerce Q into R:
-      
+
+  // Coerce Q into R:
   C:=[];
   for i:=1 to Degree(Q)+1 do
     C[i]:=Ox!(Coefficient(Q,i-1));
@@ -56,20 +59,20 @@ frob_struc:=function(data,Z,eta,bpt)
   QR:=(R!C);
 
   // Coerce z = r/LeadingCoefficient(r) into R:
-
-  zR:=(Ox!r)/LeadingCoefficient(r);
+  zR:=(Ox!r)/LeadingCoefficient(r); 
 
   // Coerce the f_i = f_{i,0}+f_{i,inf}+f_{i,end} into R:
-
   fRlist:=[];
+  finfb0list:=[];
   for i:=1 to 2*g do
-    fRlist[i]:=Qxzzinvd_to_R(compute_F(Q,W0,Winf,f0list[i],finflist[i],fendlist[i]),Q,p,r,R,W0);
+    fRlisti, finfb0list[i]:=compute_F(Q,W0,Winf,f0list[i],finflist[i],fendlist[i]);
+    fRlist[i]:=Qxzzinvd_to_R(fRlisti,Q,p,r,R,W0);
     fRlist[i]:=reduce_mod_Q(fRlist[i],QR,zR);
     fRlist[i]:=fRlist[i]-eval_R(fRlist[i],bpt,r); // make sure that f_i(bpt) = 0 
+    // No precision loss by BT17, Prop 4.5
   end for;
 
   // The matrix of Frobenius on H^1(X) is the 2gx2g top left corner of the matrix of Frobenius on H^1(U):
-
   FH1X:=ZeroMatrix(RationalField(),2*g,2*g);
   for i:=1 to 2*g do
     for j:=1 to 2*g do
@@ -78,7 +81,7 @@ frob_struc:=function(data,Z,eta,bpt)
   end for;
 
   // Compute g0:
-
+  // Z is p-integral by construction due to clearing denominators.
   A:=-Transpose(FH1X)*Z;
   A:=ChangeRing(A,R);
   g0:=[];
@@ -90,27 +93,23 @@ frob_struc:=function(data,Z,eta,bpt)
   end for;
 
   // Coerce s into R:
-
-  C:=[];
-  for i:=1 to Degree(s)+1 do
-    C[i]:=Ox!(Coefficient(s,i-1));
-  end for;
-  sR:=(R!C);
-
+  //
   // Coerce basis elements of H^1(X) into R:
+  // basis has integral coefficients by construction in basis_coho
 
   basisR:=[];
   for i:=1 to 2*g do
-      basisR[i]:=R!0;
-      for k in [1..d] do
-          //          basisR[i]:=R![S.1^0*(Ox!c) :c in Eltseq(basis[i])];
-          for j in [1..d] do
-              basisR[i]:=basisR[i]+Ox!basis[i][j]*W0S[j,k]*(R.1)^(k-1);
-              basisR[i]:=radix_reduce(basisR[i],zR);
-          end for;
+    basisR[i]:=R!0;
+    for k in [1..d] do
+      for j in [1..d] do
+        basisR[i]:=basisR[i]+Ox!basis[i][j]*W0S[j,k]*(R.1)^(k-1);
+        basisR[i]:=radix_reduce(basisR[i],zR);
       end for;
+    end for;
   end for;
 
+  // At this stage, all coefficients are integral.
+  // radix_reduce doesn't lead to prec loss by p.15 of [Tui17]
 
   // Compute g0*omega:
 
@@ -121,13 +120,9 @@ frob_struc:=function(data,Z,eta,bpt)
   g0omega:=reduce_mod_Q(g0omega,QR,zR);
   g0omega:=radix_reduce(g0omega,zR);    
   g0omega:=Vector(Coefficients(g0omega))*W0invS;
-// 16/04/20: this bit of the code is a bit odd looking: to get the write answer for the element of S^d g0omega, we have to reduce using the function radix_reduce. Since this function only reduces elements of R, we artificially make it an element of S^d by taking the dot product with [y^(j-1)], reducing, and taking coefficients.
+// 16/04/20: this bit of the code is a bit odd looking: to get the right answer for the element of S^d g0omega, we have to reduce using the function radix_reduce. Since this function only reduces elements of R, we artificially make it an element of S^d by taking the dot product with [y^(j-1)], reducing, and taking coefficients.
   g0omega:=radix_reduce(&+[g0omega[i]*(R.1)^(i-1):i in [1..d]],zR);
   g0omega:=Vector(Coefficients(g0omega));
-
-  // Precompute Fp(y^i/r) for i=0,..,3 (in fact, this has already happened inside coleman_data, but is not available here)
-
-  frobmatb0r:=froblift(Q,p,Nmax-1,r,Delta,s,W0);
 
   // determine the number of points at infinity
 
@@ -139,12 +134,12 @@ frob_struc:=function(data,Z,eta,bpt)
   end for;
 
   // Compute phi^(*)(eta)-p(eta):
-
+  kappa2 := Min([0] cat [Valuation(c, p) : c in Eltseq(eta)]);
   eta_new:=[];
   for i:=1 to d do
     sum:=PolynomialRing(RationalField())!0;
     for j:=1 to infpoints-1 do
-      sum:=sum+Qx!(eta[j]*(PolynomialRing(RationalField())!basis[2*g+j][i])); // fix this
+      sum:=sum+Qx!(eta[j]*(PolynomialRing(RationalField())!basis[2*g+j][i])); 
     end for;
     eta_new[i]:=sum;
   end for;
@@ -152,25 +147,25 @@ frob_struc:=function(data,Z,eta,bpt)
 
   phi_eta:=frobenius(eta,Q,p,Nmax,r,frobmatb0r); // phi^(*)(eta)
   for i:=1 to d do
-    phi_eta[i]:=phi_eta[i]-p*(S!Ox!eta[i]); // phi^(*)(eta)-p(eta),        as vector in S^4
+    phi_eta[i]:=phi_eta[i]-p*(S!Ox!eta[i]); // phi^(*)(eta)-p(eta),   as vector in S^4
   end for;
 
   // Compute phi^*(omega):
-
+  // No precision loss, since basis of H^1(X) is p-integral.
   phiomega:=[];
   for i:=1 to 2*g do
     phiomegai:=frobenius(basis[i],Q,p,Nmax,r,frobmatb0r);
     phiomega[i]:=R!0;
     for j:=1 to d do
-        for k in [1..d] do
-      phiomega[i]:=phiomega[i]+phiomegai[j]*W0S[j,k]*(R.1)^(k-1);
-        end for;
+      for k in [1..d] do
+        phiomega[i]:=phiomega[i]+phiomegai[j]*W0S[j,k]*(R.1)^(k-1);
+      end for;
     end for;
-        phiomega[i]:=radix_reduce(phiomega[i],zR);
+    phiomega[i]:=radix_reduce(phiomega[i],zR);
   end for;
 
   // Compute Z*f:
-
+  // No precision loss, since Z is p-integral.
   Zf:=[];
   for i:=1 to 2*g do
     Zf[i]:=R!0;
@@ -183,17 +178,16 @@ frob_struc:=function(data,Z,eta,bpt)
 
   phiomegaZf:=R!0;
   for i:=1 to 2*g do
-      phiomegaZf:=reduce_mod_Q(phiomegaZf+phiomega[i]*Zf[i],QR,zR);
+    phiomegaZf:=reduce_mod_Q(phiomegaZf+phiomega[i]*Zf[i],QR,zR);
   end for;
   phiomegaZf:=Vector(Coefficients(phiomegaZf))*W0invS;
-// 16/04/20: this looks a bit weird, but see remarks above on the computation of g0omega.
+  // 16/04/20: this looks a bit weird, but see remarks above on the computation of g0omega.
   phiomegaZf:=radix_reduce(&+[phiomegaZf[i]*(R.1)^(i-1):i in [1..d]],zR);
   phiomegaZf:=Vector(Coefficients(phiomegaZf));
-  // Compute c and h:
-  //print "convert",phiomegaZf+phi_eta-g0omega;
-  sum:=convert_to_Qxzzinvd(phiomegaZf+phi_eta-g0omega,Q);
-  //print "sum",sum;
-  c,h0,hinf,hend:=reduce_with_fs(sum,Q,p,N,Nmax,r,W0,Winf,G0,Ginf,red_list_fin,red_list_inf,basis,integrals,quo_map); 
+
+  // Compute c and h
+  chi:=convert_to_Qxzzinvd(phiomegaZf+phi_eta-g0omega,Q);
+  c,h0,hinf,hend,minf:=reduce_with_fs(chi,Q,p,N,Nmax,r,W0,Winf,G0,Ginf,red_list_fin,red_list_inf,basis,integrals,quo_map); 
   hR:=Qxzzinvd_to_R(compute_F(Q,W0,Winf,h0,hinf,hend),Q,p,r,R,W0);
   hR:=reduce_mod_Q(hR,QR,zR);
   hR:=hR-eval_R(hR,bpt,r); // make sure that h(bpt) = 0 
@@ -221,6 +215,23 @@ frob_struc:=function(data,Z,eta,bpt)
   end for;
   G[2*g+2,2*g+2]:=p;
 
-  return G;
+  // Estimate precision loss 
+  kappa_end := Min(&cat[&cat[[Valuation(c, p) : c in Coefficients(Eltseq(t)[i])] : i in [1..#Eltseq(t)]] : t in fendlist]);
+  kappa_inf := Min(&cat[&cat[[Valuation(c, p) : c in Coefficients(Eltseq(t)[i])] : i in [1..#Eltseq(t)]] : t in finflist]);
+  kappa1 := Min([kappa_inf, kappa_end]);
+  kappa := Min([kappa1, kappa2, 0]);
+  j := Nmax div 2;
+  rhs_bound := p*Nmax - p*kappa + log(p, data`e0);
+  repeat
+    j +:= 1;
+  until j - p*log(p, j) gt rhs_bound;
+  f1 := Floor(log(p, (j-1)*data`e0)) + Floor(log(p, (-ord_inf_mat(W^-1)+1)*data`einf)); // Tui17, pf of Prop 4.10
+  chiinf := change_basis_b0binf(chi,p,Nmax,r,W0,Winf);
+  prec_loss_bd := Floor(log(p, -Min([Valuation(s): s in chi])*data`e0)); // Tui17, Prop. 3.7
+  chiinf := change_basis_b0binf(chi,p,Nmax,r,W0,Winf);
+  m_inf := -Min([Valuation(d) : d in Eltseq(chiinf)]) - Degree(data`r) + 1; 
+  f2 := Floor(log(p, minf*data`einf)); // Tui17, Prop 3.8
+
+  return G, N - Max(f1, f2);
 end function;
 
--- heights.m
+++ heights.m
@@ -2,26 +2,27 @@
 // functions for computing heights //
 /////////////////////////////////////
 
+// July 21: JSM/JB added precision estimates
 
-function frob_equiv_iso(G,data)
+
+
+function frob_equiv_iso(G,data,N)
 
   // Given a matrix G defining Frob^-1 on a mixed extension this returns s^phi.
 
-  p:=data`p; N:=data`N; g:=data`g; F:=data`F;
+  p:=data`p; g:=data`g; F:=data`F; //N:=data`N; 
 
   Qp:=pAdicField(p,N);  
-
   FQp:=ZeroMatrix(Qp,2*g,2*g);
   for i:=1 to 2*g do
     for j:=1 to 2*g do
       FQp[i,j]:=Qp!F[i,j];
     end for;
   end for;
-  
+
   fx:=Matrix(Qp,2*g,1,[Qp!G[i+1,1]:i in [1..2*g]]);
   gx:=Matrix(Qp,1,2*g,[Qp!G[2*g+2,i+1]:i in [1..2*g]]);
   hx:=Matrix(Qp,1,1,[Qp!G[2*g+2,1]]);
-  
   I := IdentityMatrix(Qp,2*g);
   
   hxprime := 1/(1-p)*(gx*((I-FQp)^-1)*fx+hx);
@@ -33,21 +34,28 @@ function frob_equiv_iso(G,data)
   end for;
   s_phi[2*g+2,1] := hxprime[1,1];
 
-  return s_phi;
+  return s_phi, Min(N, minprec(s_phi));
 
 end function;
 
 
-function parallel_transport(P1,P2,Z,eta,data:prec:=0)
+function parallel_transport(P1,P2,Z,eta,data:prec:=0,N:=0)
 
   // Computes the parallel transport map of the unipotent 
   // connection Lambda defined by Z,eta from P1 to P2.
 
   // TODO: on a finite bad disk, make sure to take local parameters centered at the (very) bad point.
 
-  x1:=P1`x; x2:=P2`x; b1:=P1`b; b2:=P2`b; Q:=data`Q; p:=data`p; N:=data`N; W0:=data`W0; Winf:=data`Winf; r:=data`r; basis:=data`basis; g:=data`g;
+  x1:=P1`x; x2:=P2`x; b1:=P1`b; b2:=P2`b; Q:=data`Q; p:=data`p; W0:=data`W0; Winf:=data`Winf; r:=data`r; basis:=data`basis; g:=data`g;
   d:=Degree(Q); lc_r:=LeadingCoefficient(r); K:=Parent(x1);
 
+  if IsZero(N) then N:=data`N; end if;
+
+  C:=IdentityMatrix(K,2*g+2);
+  if are_equal_records(P1, P2) then
+    return C,data`N;
+  end if;
+    
   if not lie_in_same_disk(P1,P2,data) then
     error "the points do not lie in the same residue disk";
   end if;
@@ -113,12 +121,17 @@ function parallel_transport(P1,P2,Z,eta,data:prec:=0)
     end for;
     omegax[i]:=InnerProduct(Vector(basisxt*derxt*lc_r*denom),bt);
     omegax[i]:=Qt!Kt!omegax[i];
-    if Coefficient(omegax[i],-1) ne 0 then
-      omegax[i]:=omegax[i]-Coefficient(omegax[i],-1)*Kt.1^(-1);
-    end if;
+    assert IsZero(Coefficient(omegax[i],-1)); // second kind
   end for;
 
   // tiny single integrals
+  //
+  // compute p-adic precision
+  e:=Degree(Parent(x2)); // e=1 in our applications
+  maxpoleorder:=-(Minimum([Valuation(omegax[i]): i in [1..2*g]])); 
+  maxdegree:=Maximum([Degree(omegax[i]): i in [1..2*g]]);
+  mindegree:=Minimum([Degree(omegax[i]): i in [1..2*g]]);
+  Nsingle := tiny_integral_prec(prec,1,maxpoleorder,maxdegree,mindegree,val,data);
 
   Omegax:=[];
   for i:=1 to 2*g do
@@ -145,6 +158,13 @@ function parallel_transport(P1,P2,Z,eta,data:prec:=0)
   for i:=1 to infpoints-1 do
     dgx:=dgx-eta[i]*omegax[2*g+i];
   end for;  
+
+  val_eta := Min([0] cat [Valuation(c, p) : c in Eltseq(eta)]);
+  maxpoleorder:=-Valuation(dgx);
+  maxdegree:=Degree(dgx);
+  mindegree:=maxdegree;
+  Ndouble := Integers()!tiny_integral_prec(prec,1,maxpoleorder,maxdegree,mindegree,val,data : N := N+Min(val_eta, -Floor(log(p,N))));
+
   gx:=Integral(dgx);
 
   if index eq 0 then // x-x(P1) is the local coordinate
@@ -153,7 +173,7 @@ function parallel_transport(P1,P2,Z,eta,data:prec:=0)
     doubleintegral:=Evaluate(gx,b2[index]-b1[index]);
   end if;
 
-  C:=IdentityMatrix(K,2*g+2);
+  C:=IdentityMatrix(pAdicField(p,Ndouble), 2*g+2);
   // entries in first column (except last one) 
   for i:=1 to 2*g do
     C[i+1,1]:=-singleintegrals[i];
@@ -167,12 +187,12 @@ function parallel_transport(P1,P2,Z,eta,data:prec:=0)
   // bottom left entry
   C[2*g+2,1]:=-doubleintegral;
 
-  return C;
+  return C, Ndouble;
 
 end function;
 
 
-function parallel_transport_to_z(P,Z,eta,data:prec:=0)
+function parallel_transport_to_z(P,Z,eta,data:prec:=0,N:=0)
 
   // Computes the parallel transport map of the unipotent 
   // connection Lambda defined by Z,eta from P to to an
@@ -181,8 +201,9 @@ function parallel_transport_to_z(P,Z,eta,data:prec:=0)
   // and bt of the coordinates on the curve in terms of this 
   // local parameter are also returned.
 
-  x0:=P`x; b:=P`b; Q:=data`Q; p:=data`p; N:=data`N; basis:=data`basis; g:=data`g; r:=data`r; W0:=data`W0; Winf:=data`Winf;
+  x0:=P`x; b:=P`b; Q:=data`Q; p:=data`p; basis:=data`basis; g:=data`g; r:=data`r; W0:=data`W0; Winf:=data`Winf;
   d:=Degree(Q); lc_r:=LeadingCoefficient(r); W:=Winf*W0^(-1); K:=Parent(x0);
+  if IsZero(N) then N:=data`N; end if;
 
   P1:=P; // TODO: on a finite bad disk, make sure to take local parameters centered at the (very) bad point.
   x1:=P1`x;
@@ -235,6 +256,8 @@ function parallel_transport_to_z(P,Z,eta,data:prec:=0)
   end for;
 
   // compute the powerseries expansions of the basis elements of H^1(Y)
+  // TODO: These get recomputed a lot. Maybe store them and update if we need them to
+  // larger precision?
 
   derxt:=Qt!Kt!Derivative(xt); 
   omegax:=[];
@@ -245,20 +268,16 @@ function parallel_transport_to_z(P,Z,eta,data:prec:=0)
     end for;
     omegax[i]:=InnerProduct(Vector(basisxt*derxt*lc_r*denom),bt);
     omegax[i]:=Qt!Kt!omegax[i];
-    if Coefficient(omegax[i],-1) ne 0 then
-      omegax[i]:=omegax[i]-Coefficient(omegax[i],-1)*Kt.1^(-1);
-    end if;
+    assert IsZero(Coefficient(omegax[i],-1)); // second kind
   end for;
 
   // tiny single integrals
-
   Omegax:=[];
   for i:=1 to 2*g do
     Omegax[i]:=-Integral(omegax[i]);
   end for;
 
   // tiny double integral
-
   dgx:=0;
   for i:=1 to 2*g do
     for j:=1 to 2*g do
@@ -291,162 +310,13 @@ function parallel_transport_to_z(P,Z,eta,data:prec:=0)
 
 end function;
 
-function parallel_transport_from_b(P,Z,eta,data,kb:prec:=0,printlevel:=0)
-  //P will always be the point bQ
-
-  x0 := P`x; b:=P`b; Q:=data`Q; p:=data`p; N:=data`N; basis:=data`basis; g:=data`g; r:=data`r; W0:=data`W0; Winf:=data`Winf;
-  b0 := teichmueller_pt(P,data); 
-  d  := Degree(Q); lc_r:=LeadingCoefficient(r); W:=Winf*W0^(-1); K:=Parent(x0);
-
-  P1:=P; // TODO: on a finite bad disk, make sure to take local parameters centered at the (very) bad point.
-  x1:=P1`x;
-
-  print "P (bQ): ", P;
-
-  xt,bt,index:=local_coord(P1,prec,data); 
-
-  print "xt: ", xt;
-
-  xtold:=xt;
-  btold:=bt;
-
-  Qt<t>:=LaurentSeriesRing(RationalField(),prec);
-  xt:=Qt!xt;
-  btnew:=[Qt|];
-  for i:=1 to d do
-    btnew[i]:=Qt!bt[i];
-  end for;
-  bt:=Vector(btnew);
-
-  Kt:=LaurentSeriesRing(K,prec);
-  OK:=RingOfIntegers(K);
-  OKt:=LaurentSeriesRing(OK,prec);
-
-  if P1`inf then
-    xt:=1/xt;
-    xt:=Qt!Kt!xt; 
-    Winv:=W0*Winf^(-1);          
-    bt:=bt*Transpose(Evaluate(Winv,xt));
-    for i:=1 to d do
-      bt[i]:=Qt!(Kt!bt[i]);
-    end for; 
-  end if;
-
-  if P1`inf or not is_bad(P1,data) then 
-    denom:=Qt!Kt!(1/Evaluate(r,xt));
-  else
-    Qp:=pAdicField(p,N);
-    Qpx:=PolynomialRing(Qp);
-    rQp:=Qpx!r;
-    zero:=HenselLift(rQp,x1);
-    sQp:=rQp div (Qpx.1-zero);
-    denom:=Qt!Kt!((Qt!OKt!(xt-Coefficient(xt,0)))^(-1)*(Qt!Kt!(1/Evaluate(sQp,xt))));
-  end if;
-
-  // determine the number of points at infinity
-
-  FF:=fun_field(data);
-  infplaces:=InfinitePlaces(FF);
-  infpoints:=0;
-  for i:=1 to #infplaces do
-    infpoints:=infpoints+Degree(infplaces[i]);
-  end for;
-
-  // compute the powerseries expansions of the basis elements of H^1(Y)
-
-  derxt:=Qt!Kt!Derivative(xt); 
-  omegax:=[];
-  for i:=1 to 2*g+infpoints-1 do
-    basisxt:=Evaluate(basis[i],xt);
-    for j:=1 to d do
-      basisxt[1][j]:=Qt!Kt!basisxt[1][j];
-    end for;
-    omegax[i]:=InnerProduct(Vector(basisxt*derxt*lc_r*denom),bt);
-    omegax[i]:=Qt!Kt!omegax[i];
-    if Coefficient(omegax[i],-1) ne 0 then
-      omegax[i]:=omegax[i]-Coefficient(omegax[i],-1)*Kt.1^(-1);
-    end if;
-  end for;
-
-  // tiny single integrals
-
-  Omegax:=[];
-  for i:=1 to 2*g do
-    Omegax[i]:=-Integral(omegax[i]);
-  end for;
-
-  // tiny double integral
-
-  dgx:=0;
-  for i:=1 to 2*g do
-    for j:=1 to 2*g do
-      dgx:=dgx+omegax[i]*(K!Z[i,j])*Omegax[j]; 
-    end for;
-  end for;
-  for i:=1 to infpoints-1 do
-    dgx:=dgx-eta[i]*omegax[2*g+i];
-  end for;  
-  gx:=Integral(dgx);
-
-  C:=IdentityMatrix(Parent(gx),2*g+2);
-
-  // entries in first column (except last one) 
-  for i:=1 to 2*g do
-    C[i+1,1]:= Omegax[i]; 
-  end for;
-
-  // entries in the last row (except first one)
-  for i:=1 to 2*g do
-    for j:=1 to 2*g do
-      C[2*g+2,i+1]:=(C[2*g+2,i+1]-Omegax[j]*(K!Z[j,i]));
-    end for;
-//     C[2*g+2,i+1] := - C[2*g+2,i+1];
-  end for;
-  // bottom left entry
-  C[2*g+2,1]:= gx; 
-
-  xt:=xtold;
-  bt:=Vector(btold);
-
-  print "xt: ", xt;
-  param:=(b0`x - P`x);  
-  if printlevel gt 1 then
-    print "param: ", param;
-    print "Evaluating xt at param: ", Evaluate(xt,param);
-    print "b0's x-coord: ", b0`x;
-  end if;
-  for i :=1 to 2*g+2 do
-    for j:=1 to 2*g+2 do
-       C[i,j]:= Evaluate(C[i,j],param);
-    end for;
-  end for;
-  
-  return C,xt,bt;
-
-end function;
-
-
-// function parallel_transport_from_b2(P,Z,eta,data,kb:prec:=0)
-// 
-// 	x0:=P`x; b:=P`b; Q:=data`Q; p:=data`p; N:=data`N; basis:=data`basis; g:=data`g; r:=data`r; W0:=data`W0; Winf:=data`Winf;
-// 	b0:=teichmueller_pt(P,data); 
-// 	d:=Degree(Q); lc_r:=LeadingCoefficient(r); W:=Winf*W0^(-1); K:=Parent(x0);
-// 	
-// 	C := parallel_transport(b0,P,Z,eta,data:prec:=prec);
-// 	for i := 1 to 2*g do
-// 		C[2*g+2,i+1] := -C[2*g+2,i+1];
-// 	end for;
-// 
-// 	return C;
-// 
-// end function;
-
-
 
 height:=function(Phi,betafil,gammafil,splitting,data)
 
-  // This function computes the p-adic height of a filtered phi-module given its Frobenius 
+  // This function computes the local p-adic height at p of a 
+  // filtered phi-module given its Frobenius 
   // matrix Phi and splitting of the Hodge filtration determined by gamma_fil, beta_fil.
+
 
   p:=data`p; g:=data`g;
 
@@ -467,14 +337,10 @@ height:=function(Phi,betafil,gammafil,splitting,data)
 end function;
 
 
-E1_tensor_E2:=function(Phi,betafil,basisH0star,data,Salpha)
+E1_tensor_E2:=function(Phi,betafil,changebasis,data,Salpha)
 
-  // TODO comment
-
-  g:=data`g;
-  changebasis:=Matrix(basisH0star)^(-1);
   changebasis:=ChangeRing(changebasis,Salpha);
-
+  g:=data`g;
   E1 := Vector(Salpha,[Phi[i,1] : i in [2..g+1]])*changebasis; 
   E2 := Vector(Salpha,[Phi[2*g+2,g+1+j] - betafil[j] : j in [1..g]])*changebasis;
 
@@ -498,6 +364,7 @@ expand_algebraic_function:=function(P,g,data,N,prec)
 end function;
 
 
+
 function height_coefficients(divisors, intersections, local_CG_heights_p, data)
   // TODO: arbitrary g
 
@@ -515,16 +382,11 @@ function height_coefficients(divisors, intersections, local_CG_heights_p, data)
   end for;
 
   heights_basis := [local_CG_heights_p[i] + local_CG_heights_away[i] : i in [1..#local_CG_heights_p]];
-  //long_divisors := [divisors[1], divisors[1], divisors[1], divisors[2], divisors[2], divisors[2]];
-  //alphas := height_fitting_g2(long_divisors, heights_basis, data);
 
   n := g*(g+1) div 2;
   M := ZeroMatrix(Parent(divisors[1,1,1,1]), n, n);
 
   integrals := [coleman_integrals_on_basis_divisors(t[2], t[1], data) : t in divisors];
-  //for i,j in [1..N] do
-    //M[i,j] := 1/2*(integrals[1,1]*integrals[2,2]+integrals[2,1]*integrals[1,2]); 
-  //end for;
   M[1,1] := integrals[1,1]*integrals[1,1];
   M[1,2] := 1/2*(integrals[1,1]*integrals[1,2]+integrals[1,1]*integrals[1,2]);
   M[1,3] := integrals[1,2]*integrals[1,2];
@@ -540,5 +402,60 @@ function height_coefficients(divisors, intersections, local_CG_heights_p, data)
 
   alphas := Eltseq(M^-1*heights_vector);
   return [[alphas[1], alphas[2]], [0, alphas[3]]];
+end function;
+
+
+
+function new_height_coefficients(divisors, intersections, factors, local_CG_heights_p, data)
+  // TODO: arbitrary g
+  // Same as height_coefficients, but allow more general divisors.
+
+
+  g := data`g;
+  if g ne 2 then
+    error "Only genus 2 currently implemented";
+  end if;
+
+  p := data`p;
+  Qp := pAdicField(p, data`N);
+
+  local_CG_heights_away := [ ];
+  for i := 1 to #intersections do
+   local_CG_heights_away[i] := IsEmpty(intersections[i]) select 0
+           else &+[Log(Qp!t[1])*t[2] : t in intersections[i] | t[1] ne p];
+  end for;
+
+  factors1, factors2 := Explode(factors);
+  heights_basis := [local_CG_heights_p[i] - local_CG_heights_away[i] : i in [1..#local_CG_heights_p]];
+  h1 := heights_basis[1]/(factors1[1,1]*factors2[1,1]);
+  h2 := heights_basis[2]/(factors1[1,2]*factors2[1,2]);
+  h3 := heights_basis[3]/(factors1[2,2]*factors2[2,2]);
+  regulator := h1*h3-h2^2;
+
+  n := g*(g+1) div 2;
+  M := ZeroMatrix(Parent(divisors[1,1,1,1]), n, n);
+
+  
+  integrals := [coleman_integrals_on_basis_divisors(t[2], t[1], data) : t in divisors];
+  integrals[1,1] /:= factors1[1,1];
+  integrals[1,2] /:= factors1[1,1];
+  integrals[2,1] /:= factors2[2,2];
+  integrals[2,2] /:= factors2[2,2];
+
+  M[1,1] := integrals[1,1]*integrals[1,1];
+  M[1,2] := 1/2*(integrals[1,1]*integrals[1,2]+integrals[1,1]*integrals[1,2]);
+  M[1,3] := integrals[1,2]*integrals[1,2];
+  M[2,1] := integrals[1,1]*integrals[2,1];
+  M[2,2] := 1/2*(integrals[1,1]*integrals[2,2]+integrals[2,1]*integrals[1,2]);
+  M[2,3] := integrals[1,2]*integrals[2,2];
+  M[3,1] := integrals[2,1]*integrals[2,1];
+  M[3,2] := 1/2*(integrals[2,1]*integrals[2,2]+integrals[2,1]*integrals[2,2]);
+  M[3,3] := integrals[2,2]*integrals[2,2];
+
+  assert Determinant(M) ne 0;  // Points should be independent
+  heights_vector := Matrix(BaseRing(M),n, 1, [h1,h2,h3]);
+
+  alphas := Eltseq(M^-1*heights_vector);
+  return [[alphas[1], alphas[2]], [0, alphas[3]]], regulator;
 end function;
 
