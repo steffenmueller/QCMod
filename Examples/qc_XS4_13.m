ca8289012dc44d679fcb82e638306a83b854ee62
Steffen Müller <jansteffenmueller@gmail.com>
2022-01-05 11:52:54 +0100

    removed comments

--- Examples/qc_XS4_13.m
+++ Examples/qc_XS4_13.m
@@ -1,15 +1,13 @@
 SetLogFile("qc_XS4_13.log");
 load "qc_modular.m";
-//load "howe_zhu.m";
 
 // We use the model of X_S4(13) found by Banwait-Cremona in `Tetrahedral elliptic curves
 // and the local-global principle for isogenies`. Algebra & Number Theory, 8(5):1201–1229, 2014
 Q_S4 := 4*x^3*y - x^3- 3*x^2*y^2 + 16*x^2*y+ 3*x^2+ 3*x*y^3 - 11*x*y^2+ 9*x*y+x+ 5*y^3+ y^2+ 2*y;
 // We know that the Jacobian of X_S4(13) is isogenous to the Jacobian of X0(169)+, so
 // the fact that the rank is 3 and that the Jacobian is absolutely simple follow from
-// the analogous results in qc_Xs(13)plus.m
+// the analogous results in qc_Xs13plus.m
 //
-//TODO: Need to check that the Hecke operator at 11 generates. Are they in Banwait-Cremona?
 
 // Find a good affine covering. For this, we apply second_affine_patch twice.
 p := 11; 
